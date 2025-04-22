4ceea99c04b059e6c08c025a51520a70bdaf133d
Jonas Andersson <jonas.andersson.web@gmail.com>
2025-04-22 22:02:52 +0200

    Added header from DaisyUI

---
+++ src/lib/components/composites/header/header.svelte
@@ -0,0 +1,5 @@
+<div class="navbar bg-base-100 justify-center shadow-sm">
+    <div class="navbar-center">
+        <a class="btn btn-ghost text-xl" href="/">Dummy Shop</a>
+    </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+    import Header from '$lib/components/composites/header/header.svelte';
     import '../app.css';
 
     let { children } = $props();
 </script>
 
-{@render children()}
+<Header />
+
+<main class="mx-auto mt-14 max-w-7xl px-4 sm:px-6 lg:px-8">
+    {@render children()}
+</main>
